7d0e13329a2582e85f41ef845874f8088127937f
luisito <luarmoro123@gmail.com>
2025-10-12 20:55:53 -0700

    se incio la app

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen } from 'expo-router'
+import "./global.css"
+import { useFonts } from 'expo-font'
+
+
+SplashScreen.preventAutoHideAsync()
+
+const RootLayout = () => {
+    const [fontLoaded, error] = useFonts({
+        'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+        'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+        'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+
+    })
+
+    useEffect(() => {
+        if (error) throw error
+        if (fontLoaded) SplashScreen.hideAsync()
+
+
+    }, [fontLoaded, error])
+
+    if (!fontLoaded && !error) return null
+
+    return (
+        <Slot />
+    )
+}
+
+export default RootLayout
---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
+
+const index = () => {
+    return (
+        <SafeAreaView>
+            <View className='mt-6 mx-5'>
+                <Text className='text-4xl '
+                    style={{ fontFamily: 'WorkSans-Black' }}>Hola mundo</Text>
+                <Text className='text-3xl font-work-black text-primary'>Hola mundo</Text>
+                <Text className='text-2xl font-work-medium text-secondary'>Hola mundo</Text>
+                <Text className='text-xl font-work-light text-tertiary'>Hola mundo</Text>
+                <Text className='text-xl '>Hola mundo</Text>
+
+
+                <Link href='/products'>
+                    Products
+                </Link>
+            </View>
+        </SafeAreaView>
+
+    )
+}
+
+export default index
